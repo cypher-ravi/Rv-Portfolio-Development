8c70c5277f34cd7cb9f8c69393d2958a14453a39
ravi <ronniloreo@gmail.com>
2021-10-22 22:48:30 -0700

    uploading project

--- Portfolio/settings/production.py
+++ Portfolio/settings/production.py
@@ -26,16 +26,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB-NAME'),
+#         'USER': config('DB-USER'),
+#         'PASSWORD': config('DB-PASSWORD'),
+#         'HOST': config('DB-HOST'),
+#         'PORT': config('DB-PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB-NAME'),
-        'USER': config('DB-USER'),
-        'PASSWORD': config('DB-PASSWORD'),
-        'HOST': config('DB-HOST'),
-        'PORT': config('DB-PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'), 
     }
 }
 
-STRIPE_PUBLIC_KEY = config('STRIPE-PUBLIC-KEY')
-STRIPE_SECRET_KEY = config('STRIPE-SECRET-KEY') 
+# STRIPE_PUBLIC_KEY = config('STRIPE-PUBLIC-KEY')
+# STRIPE_SECRET_KEY = config('STRIPE-SECRET-KEY') 
